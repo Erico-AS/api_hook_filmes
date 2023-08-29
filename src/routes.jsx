f06795e1cec86c27a67a86e79208fb844cb82fc0
Erico-AS <ericoaugustosstj@gmail.com>
2023-08-29 12:26:18 -0300

    Start

---
+++ src/routes.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/home'
+import FilmesErico from './pages/erico'
+
+export default function AppRoutes() {
+    return (<>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/erico" element={<FilmesErico />}></Route>
+                <Route path="/melo"></Route>
+            </Routes>
+        </BrowserRouter>
+        </>)
+}

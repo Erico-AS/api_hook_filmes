a06ece6ed5a70698258264dd05da2587d40a8fd0
Melituzzz <99284305+MelodiasGabriel@users.noreply.github.com>
2023-09-01 08:45:59 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import AppRoutes from './routes'
+import React from 'react'
 
 function App() {
   return (
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react'
 
 const Home = ({ setMovieData }) => {
     const [movies, setMovies] = useState([])
-    const url = `./filmes-gabriel.json`
+    const url = `https://github.com/Erico-AS/api/blob/main/melo.json`
 
     useEffect(() => {
         const getMovies = async () => {
             const response = await fetch(url)
-            /* console.log(response) */
+            console.log(response)
             const data = await response.json()
-            /* console.log(data) */
+            console.log(data)
             setMovies(data)
         }
         getMovies()

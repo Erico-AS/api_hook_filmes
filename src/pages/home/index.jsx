f4c7901d88aa97215d6500305b6f24402617237d
Érico Augusto <67467324+Erico-AS@users.noreply.github.com>
2023-09-01 08:21:16 -0300

    Update index.jsx (home)

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,39 @@
-import './styles.css'
+import React, { useEffect, useState } from 'react'
 
-export default function Home() {
-    return(
-        <h1>Home</h1>
-    )
+const Home = ({ setMovieData }) => {
+    const [movies, setMovies] = useState([])
+    const url = `./filmes-gabriel.json`
+
+    useEffect(() => {
+        const getMovies = async () => {
+            const response = await fetch(url)
+            /* console.log(response) */
+            const data = await response.json()
+            /* console.log(data) */
+            setMovies(data)
+        }
+        getMovies()
+    }, [])
+
+    console.log(movies)
+
+  return (
+    <div>
+      { 
+        movies.map( movie => (
+            <div key={movie.id}>
+                <h2>
+                    {movie.id} {movie.titulo} <br/> {movie.data}
+                    {movie.diretores} <br/> {movie.duracao} <br/> {movie.avaliacao} <br/>
+                    {movie.tipo} <br/> {movie.sinopse} <br/> {movie.imagem}
+                </h2>
+                <img src={movie.imagem} alt={movie.titulo} />
+            </div>
+        ))
+      
+      }
+    </div>
+  )
 }
+
+export default Home

12f0fa702d173471150d2ba51d70913b2f2c138e
Melituzzz <99284305+MelodiasGabriel@users.noreply.github.com>
2023-09-05 07:37:50 -0300

    Add files via upload

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,12 +3,22 @@ import { Link } from 'react-router-dom'
 import { Button } from '@mui/material'
 
 const Home = ({ setMovieData }) => {
-/*    const [movies, setMovies] = useState([])
-    
-    useEffect(() => {
-        getMovies()
-    }, [])*/
+  const [movies, setMovies] = useState([])
+  const url = `https://raw.githubusercontent.com/Erico-AS/api_hook_filmes/main/data/filmes.json`
 
+  useEffect(() => {
+      const getMovies = async () => {
+          const response = await fetch(url)
+          console.log(response)
+          const data = await response.json()
+          console.log(data)
+          setMovies(data)
+      }
+      getMovies()
+  }, [])
+
+  console.log(movies.categoria)
+  
   return (
     <div>
         <Button variant="contained"><Link to="/erico">Filmes Erico</Link></Button>
